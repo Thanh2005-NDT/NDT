2c7a1f10a4b7c049c89dc4f87c36f3f61f6f5bcb
Thanh2005-NDT <vu261195@gmail.com>
2025-11-03 15:27:49 +0700

    Update thamso.py

--- ACO/thamso.py
+++ ACO/thamso.py
@@ -1,30 +1,20 @@
 import math
-def doc_file(ten_file):
-    with open(ten_file, "r") as f:
-        lines = [line.strip() for line in f if line.strip()]
-    n = int(lines[0])
-    toa_do = [tuple(map(float, line.split())) for line in lines[1:]]
+
+def docfile(tenfile):
+    with open(tenfile, "r") as f:
+        dong = [line.strip() for line in f if line.strip()]
+    n = int(dong[0])
+    toado = [tuple(map(float, line.split())) for line in dong[1:]]
     kc = [[0]*n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             if i != j:
-                kc[i][j] = math.dist(toa_do[i], toa_do[j])
-    return n, kc, toa_do
+                kc[i][j] = math.dist(toado[i], toado[j])
+    return n, kc, toado
 
-
-# kc = [
-#     [0, 6, 10, 8, 5],
-#     [6, 0, 8, 10, 5],
-#     [10, 8, 0, 6, 5],
-#     [8, 10, 6, 0, 5],
-#     [5, 5, 5, 5, 0]
-# ]
-
-# Các tham số của ACO
-n = 5          # số thành phố
-m = 5          # số kiến
-alpha = 1      # trọng số pheromone
-beta = 2       # trọng số heuristic
-rho = 0.5      # hệ số bay hơi pheromone
-Q = 100        # lượng pheromone gửi về
-so_vong = 20   # số vòng lặp
+m = 5         # so kien
+alpha = 1     # trong so pheromone
+beta = 2      # trong so heuristic
+rho = 0.5     # he so bay hoi
+Q = 100       # hang so pheromone
+vong = 20     # so vong lap
